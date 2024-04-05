c853688c468eacd023830334178298902224990b
Angelica <Angelica137@users.noreply.github.com>
2024-04-05 14:01:45 +0100

    first_non_repeating_letter returns first non repeating char, red

---
+++ tests/test_first_non_repeating_letter.py
@@ -0,0 +1,7 @@
+from test_first_non_repeating_letter import *
+
+
+def test_first_non_repeat_returns_first_non_repeat_char():
+    """tests the first non repeated cahr is returned"""
+    string = "Hello"
+    assert first_non_repeating_letter == "H"
