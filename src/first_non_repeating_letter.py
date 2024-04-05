3cc4e919750e986862aaf01c06d8156772a67c9b
Angelica <Angelica137@users.noreply.github.com>
2024-04-05 14:23:46 +0100

    correct for case sensitivity, do not modify char, green

--- src/first_non_repeating_letter.py
+++ src/first_non_repeating_letter.py
@@ -1,11 +1,12 @@
 def first_non_repeating_letter(word: str) -> str:
     count = {}
     for char in word:
+        char = char.lower()
         if char in count:
             count[char] += 1
         else:
             count[char] = 1
     for char in word:
-        if count[char] == 1:
+        if count[char.lower()] == 1:
             return char
     return "No non-repeated letters"
